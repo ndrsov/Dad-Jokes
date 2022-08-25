9425e8a68eada45e0b9830950d1ebd33a4a587bf
Andres Soria <andres.zov@gmail.com>
2022-08-25 17:52:06 -0500

    Refactor API request into an async function

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ jokeBtn.addEventListener("click", generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.textContent = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.textContent = data.joke;
 }
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.textContent = data.joke;
+//     });
+// }
